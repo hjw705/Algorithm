1963e875d8da8490d644ee2faa89beba2398b8cf
JianHan <luckywon88@gmail.com>
2021-11-25 21:08:25 +0900

    Update 최대공약수와 최소공배수.py

--- 프로그래머스/최대공약수와 최소공배수.py
+++ 프로그래머스/최대공약수와 최소공배수.py
@@ -1,17 +1,20 @@
 #level1_최대공약수와 최소공배수
-def solution(n, m):
-    answer =[]
-    #최대공약수
-    for i in range(min(n,m), 0 , -1):
-        if n%i==0 and m%i==0:
-            answer.append(i)
-            break
+def gcd(n1,n2):
+    if n1 < n2: 
+        if n2 % n1 == 0:
+            return n1
+        else:
+          return gcd(n1, int(n2%n1))
+    else:
+        if n1 % n2 == 0:
+            return n2
+        else:
+          return gcd(n2, int(n1%n2))
 
-    #최소공배수
-    for i in range(max(n,m), n*m+1):
-        if i%n == 0 and i%m==0:
-            answer.append(i)
-            return answer
+def solution(n, m):
+    gcd(n,m)
+    answer = [gcd(n,m), int(n*m / gcd(n,m))]
+    return answer
 
 #더 간단한 코드
 # def solution(n, m):
